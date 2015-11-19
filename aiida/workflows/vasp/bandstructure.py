c4db6d95dd53a283acab08c5e98502be5bdc699d
Rico Häuselmann <r.haeuselmann@gmx.ch>
2015-11-19 18:05:15 +0100

    fix syntax in bandstructure workflow

--- aiida/workflows/vasp/bandstructure.py
+++ aiida/workflows/vasp/bandstructure.py
@@ -21,90 +21,91 @@ class Bandstructure(Workflow):
         self.name = kwargs.get('name') or 'unnamed'
         super(Bandstructure, self).__init__(**kwargs)
 
-        def get_vasp_calc(self, sc=True):
-            '''
-            return an asevasp code instance.
-            kwargs:
-                sc=True: if True, sets scheduler parameters as for a selfconsistent run.
-                         aka: max walltime = 60, no mpi, single core.
-            '''
-            from aiida.orm import Code
+    def get_vasp_calc(self, sc=True):
+        '''
+        return an asevasp code instance.
+        kwargs:
+            sc=True: if True, sets scheduler parameters as for a selfconsistent run.
+                        aka: max walltime = 60, no mpi, single core.
+        '''
+        from aiida.orm import Code
 
-            vaspcalc = Code.get_from_string('vasp.asevasp')
-            vaspcalc.label = self.name
-            if self.queue:
-                vaspcalc.set_queue_name(self.queue)
-            if sc:
-                vaspcalc.set_max_wallclock_seconds(60)
-                vaspcalc.set_withmpi(False)
-                vaspcalc.set_resources({'num_machines':1, 'num_mpiprocs_per_machine':1})
+        vaspcalc = Code.get_from_string('vasp.asevasp')
+        vaspcalc.label = self.name
+        if self.queue:
+            vaspcalc.set_queue_name(self.queue)
+        if sc:
+            vaspcalc.set_max_wallclock_seconds(60)
+            vaspcalc.set_withmpi(False)
+            vaspcalc.set_resources({'num_machines':1, 'num_mpiprocs_per_machine':1})
 
-        def get_formula(self):
-            params = self.get_parameters()
-            return params['structure'].get_formula()
+    def get_formula(self):
+        params = self.get_parameters()
+        return params['structure'].get_formula()
 
-        @Workflow.step
-        def start(self):
-            '''
-            prepare, store and attach the selfconsistent run to get the charge density.
-            '''
-            params = self.get_parameters()
-            calc = get_vasp_calc(sc=True)
-            
-            calc.use_settings(params['incar'])
-            calc.use_structure(params['structure'])
-            calc.use_potentials(params['potentials'])
-            calc.use_kpoints(params['sc_kpoints'])
-            calc.use_chgcar(ParameterData(dict={}))
-            formula = self.get_formula()
-            calc.description = '{}: selfconsistent run for {}'.format(self.name, formula)
-            calc.store_all()
-            self.attach_calculation(calc)
-            self.append_to_report('{}: starting selfconsistent run, PK={}, uuid={}'.format(
-                self.name, calc.pk, calc.uuid))
-            self.add_attributes({'scstep': {'pk': calc.pk, 'uuid': calc.uuid}})
-            self.next(self.bandrun)
+    @Workflow.step
+    def start(self):
+        '''
+        prepare, store and attach the selfconsistent run to get the charge density.
+        '''
+        params = self.get_parameters()
+        calc = get_vasp_calc(sc=True)
 
-        def bandrun(self):
-            '''
-            prepare, store and attach the non-selfconsistent run to get the band structure.
-            '''
-            # get chgcar from previous step
-            scstep = self.get_attributes()['scstep']
-            prev = self.get_step_calculations(self.start)
-            if prev:
-                sccalc = [i for i in prev if prev.uuid == scstep.uuid][0]
-                self.append_to_report('{}: retrieved sc step calculation'.format(self.name))
+        calc.use_settings(params['incar'])
+        calc.use_structure(params['structure'])
+        calc.use_potentials(params['potentials'])
+        calc.use_kpoints(params['sc_kpoints'])
+        calc.use_chgcar(ParameterData(dict={}))
+        formula = self.get_formula()
+        calc.description = '{}: selfconsistent run for {}'.format(self.name, formula)
+        calc.store_all()
+        self.attach_calculation(calc)
+        self.append_to_report('{}: starting selfconsistent run, PK={}, uuid={}'.format(
+            self.name, calc.pk, calc.uuid))
+        self.add_attributes({'scstep': {'pk': calc.pk, 'uuid': calc.uuid}})
+        self.next(self.bandrun)
 
-            ichg = sccalc.out.chgcar.copy()
+    @Workflow.step
+    def bandrun(self):
+        '''
+        prepare, store and attach the non-selfconsistent run to get the band structure.
+        '''
+        # get chgcar from previous step
+        scstep = self.get_attributes()['scstep']
+        prev = self.get_step_calculations(self.start)
+        if prev:
+            sccalc = [i for i in prev if prev.uuid == scstep.uuid][0]
+            self.append_to_report('{}: retrieved sc step calculation'.format(self.name))
 
-            # set up band structure step
-            params = self.get_parameters()
-            calc = get_vasp_calc(sc=False)
+        ichg = sccalc.out.chgcar.copy()
 
-            incar = params['incar'].get_dict()
-            incar['icharg'] = 11
-            settings = ParameterData(dict=incar)
-            calc.use_settings(settings)
-            calc.use_structure(params['structure'])
-            calc.use_potentials(params['potentials'])
-            calc.use_kpoints(params['bs_kpoints'])
-            calc.use_chgcar(ichg)
+        # set up band structure step
+        params = self.get_parameters()
+        calc = get_vasp_calc(sc=False)
 
-            formula = self.get_formula()
-            calc.description = '{}: bandstructure run for {}'.format(self.name, formula)
-            calc.set_max_wallclock_seconds(1800)
-            calc.set_withmpi(True)
-            nbands = incar.get('nbands') or incar.get('NBANDS')
-            resources = {}
-            resources['num_machines'] = nbands / 10 + (nbands%10 and 1 or 0)
-            resources['tot_num_mpiprocs'] = nbands
-            calc.set_resources(resources)
-            calc.store_all()
+        incar = params['incar'].get_dict()
+        incar['icharg'] = 11
+        settings = ParameterData(dict=incar)
+        calc.use_settings(settings)
+        calc.use_structure(params['structure'])
+        calc.use_potentials(params['potentials'])
+        calc.use_kpoints(params['bs_kpoints'])
+        calc.use_chgcar(ichg)
 
-            self.attach_calculation(calc)
-            self.append_to_report('{}: starting band structure run, PK={}, uuid={}'.format(
-                self.name, calc.pk, calc.uuid))
-            self.add_attributes({'bsstep': {'pk': calc.pk, 'uuid': calc.uuid}})
-            self.set_result('band_run', calc)
-            self.next(self.exit)
+        formula = self.get_formula()
+        calc.description = '{}: bandstructure run for {}'.format(self.name, formula)
+        calc.set_max_wallclock_seconds(1800)
+        calc.set_withmpi(True)
+        nbands = incar.get('nbands') or incar.get('NBANDS')
+        resources = {}
+        resources['num_machines'] = nbands / 10 + (nbands%10 and 1 or 0)
+        resources['tot_num_mpiprocs'] = nbands
+        calc.set_resources(resources)
+        calc.store_all()
+
+        self.attach_calculation(calc)
+        self.append_to_report('{}: starting band structure run, PK={}, uuid={}'.format(
+            self.name, calc.pk, calc.uuid))
+        self.add_attributes({'bsstep': {'pk': calc.pk, 'uuid': calc.uuid}})
+        self.set_result('band_run', calc)
+        self.next(self.exit)
