2946289dc73c3eadda6e47b4f3543f8457a783c7
DropD <r.haeuselmann@gmx.ch>
2015-12-30 02:15:42 +0100

    pep8 for incar.py

--- aiida/orm/calculation/job/vasp/incar.py
+++ aiida/orm/calculation/job/vasp/incar.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def _incarify(value):
     result = None
     if isinstance(value, str):
@@ -13,20 +14,27 @@ def _incarify(value):
         elif dim == 2:
             result = '\n'.join([_incarify(i) for i in value])
         elif dim > 2:
-            raise TypeError('you are trying to input a more than 2-dimensional array to VASP. Not sure what to do...')
+            raise TypeError('you are trying to input a more ' +
+                            'than 2-dimensional array to VASP.' +
+                            'Not sure what to do...')
     elif isinstance(value, bool):
         result = value and '.True.' or '.False.'
     elif np.isreal(value):
         result = '{}'.format(value)
     return result
 
+
 def _incar_item(key, value):
     return _incar_item.tpl.format(key=key.upper(), value=_incarify(value))
 _incar_item.tpl = '{key} = {value}'
 
-def par_to_incar(incar_pardat):
+
+def dict_to_incar(incar_dict):
     incar_content = ''
-    incar_dict = incar_pardat.get_dict()
-    for k,v in incar_dict.iteritems():
+    for k, v in incar_dict.iteritems():
         incar_content += _incar_item(k, v) + '\n'
-    return incar_content
+
+
+def par_to_incar(incar_pardat):
+    incar_dict = incar_pardat.get_dict()
+    return dict_to_incar(incar_dict)
