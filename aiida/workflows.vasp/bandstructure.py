fc74a6c07bd2bef40c02457cb9bec227ba28a55b
Rico Häuselmann <r.haeuselmann@gmx.ch>
2016-01-23 11:47:15 +0100

    work on bandstructure wf.

--- aiida/workflows.vasp/bandstructure.py
+++ aiida/workflows.vasp/bandstructure.py
@@ -2,6 +2,7 @@
 from aiida.common import aiidalogger
 from aiida.orm.workflow import Workflow
 from aiida.orm import DataFactory, load_node
+from aiida.orm.calculation.job.vasp.maker import VaspMaker
 
 logger = aiidalogger.getChild('Bandstructure')
 ParameterData = DataFactory('parameter')
@@ -16,43 +17,22 @@ class Bandstructure(Workflow):
           selfconsistent run
     '''
     def __init__(self, **kwargs):
-
-        #~ self.queue = kwargs.get('queue')
-        #~ params.get('name') = kwargs.get('name') or 'unnamed'
-        #~ self.infiles = kwargs['infiles']
         super(Bandstructure, self).__init__(**kwargs)
 
-    def get_structure(self, sdict):
-        SData = DataFactory('structure')
-        st = SData(cell = sdict['cell'])
-        for atom in sdict['atoms']:
-            st.append_atom(position = atom['position'], symbols = atom['symbols'])
-        return st
-
-    def get_vasp_calc(self, sc=True):
+    def get_calc_maker(self):
         '''
-        return an asevasp code instance.
-        kwargs:
-            sc=True: if True, sets scheduler parameters as for a selfconsistent run.
-                        aka: max walltime = 60, no mpi, single core.
+        return an VaspMaker instance.
         '''
         from aiida.orm import Code
-
         params = self.get_parameters()
-        vaspcode = Code.get_from_string('asevasp')
-        vaspcalc = vaspcode.new_calc()
-        vaspcalc.label = params.get('name')
-        if params.get('queue'):
-            vaspcalc.set_queue_name(params['queue'])
-        if sc:
-            vaspcalc.set_max_wallclock_seconds(600)
-            vaspcalc.set_withmpi(False)
-            vaspcalc.set_resources({'num_machines':1, 'num_mpiprocs_per_machine':1})
-        return vaspcalc
-
-    def get_formula(self):
-        params = self.get_parameters()
-        return load_node(params['structure']).get_formula()
+        maker = VaspMaker(params['structure'])
+        maker.add_settings(icharg=0, istart-0, lorbit=11, lsorbit=True,
+                sigma=0.05, ismear=0, gga='PE', gga_compat=False)
+        maker.code = Code.get_from_string(params['code'])
+        maker.computer = maker.code.get_computer()
+        maker.label = params.get('name')
+        maker.queue = params['queue']
+        return maker
 
     @Workflow.step
     def start(self):
@@ -60,14 +40,10 @@ class Bandstructure(Workflow):
         prepare, store and attach the selfconsistent run to get the charge density.
         '''
         params = self.get_parameters()
-        calc = self.get_vasp_calc(sc=True)
+        maker = self.get_calc_maker()
 
-        calc.use_settings(load_node(params['incar']))
-        calc.use_structure(load_node(params['structure']))
-        calc.use_potentials(load_node(params['potentials']))
-        calc.use_kpoints(load_node(params['sc_kpoints']))
-        calc.use_chgcar(ParameterData(dict={}))
-        formula = self.get_formula()
+        maker.kpoints.set_kpoints_mesh([8, 8, 8])
+        calc = calc.new()
         calc.description = '{}: selfconsistent run for {}'.format(params.get('name'), formula)
         calc.store_all()
         self.attach_calculation(calc)
@@ -85,30 +61,17 @@ class Bandstructure(Workflow):
         # get chgcar from previous step
         scstep = self.get_attributes()['scstep']
         prev = self.get_step_calculations(self.start)
-        if prev:
-            #~ sccalc = [i for i in prev if i.uuid == scstep['uuid']][0]
-            sccalc = prev.get(uuid=scstep['uuid'])
-            self.append_to_report('{}: retrieved sc step calculation'.format(params.get('name')))
+        sccalc = prev.get(uuid=scstep['uuid'])
+        self.append_to_report('{}: retrieved sc step calculation'.format(params.get('name')))
 
-        ichg = sccalc.out.chgcar.copy()
+        ichgdens = sccalc.out.chgcar
 
         # set up band structure step
-        calc = self.get_vasp_calc(sc=False)
-
-        incar = load_node(params['incar']).get_dict()
-        incar['icharg'] = 11
-        settings = ParameterData(dict=incar)
-        calc.use_settings(settings)
-        calc.use_structure(load_node(params['structure']))
-        calc.use_potentials(load_node(params['potentials']))
-        calc.use_kpoints(load_node(params['sc_kpoints']))
-        #~ structure = ParameterData(dict=params['structure'])
-        #~ calc.use_structure(self.get_structure(params['structure']))
-        #~ potentials = ParameterData(dict=params['potentials'])
-        #~ calc.use_potentials(potentials)
-        #~ kpoints = ParameterData(dict=params['bs_kpoints'])
-        #~ calc.use_kpoints(kpoints)
-        calc.use_chgcar(ichg)
+        maker = self.get_calc_maker()
+        maker.rewrite_settings(icharg=11)
+        maker.kpoints.set_kpoints_path()
+        
+        maker._charge_density = ichgdens
 
         formula = self.get_formula()
         calc.description = '{}: bandstructure run for {}'.format(params.get('name'), formula)
