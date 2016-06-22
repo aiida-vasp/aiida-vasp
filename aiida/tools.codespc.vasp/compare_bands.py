5510d3d22078205cf969cbc80930a906a3256f88
Rico Häuselmann <r.haeuselmann@gmx.ch>
2016-06-23 00:59:37 +0200

    further work on band comparison, add bandgap function.

--- aiida/tools.codespc.vasp/compare_bands.py
+++ aiida/tools.codespc.vasp/compare_bands.py
@@ -7,46 +7,105 @@ ArrayData = DataFactory('array')
 
 
 @make_inline
-def compare_bands(wannier_bands, vasp_bands):
+def make_refrerence_bands(wannier_bands, vasp_bands, efermi):
+    '''
+    Compare bandstructure results from wannier and vasp.
+    Takes two input array.bands nodes, stores them if they're not already
+    stored.
+    Takes the relevant bands from the vasp bandstructure and stores and outputs
+    them in a node with linkname 'bandcmp'.
+    Also returns a parameter data node with linkname 'bandinfo' containing
+    fermi energy, bandgap etc of the reference bandstructure.
+    '''
+    import numpy as np
     assert(isinstance(wannier_bands, BandsData))
     assert(isinstance(vasp_bands, BandsData))
     assert(hasattr(wannier_bands, 'labels'))
     assert(hasattr(vasp_bands, 'labels'))
-    assert(vasp_bands.labels == wannier_bands.labels)
+    if vasp_bands.labels:
+        assert(vasp_bands.labels == wannier_bands.labels)
+    kpcomp = vasp_bands.get_kpoints() == wannier_bands.get_kpoints()
+    assert(kpcomp.all())
 
     try:
         calc = wannier_bands.inp.bands
         wset = calc.inp.settings.get_dict()
         owindow = (
-            wset.get('dis_window_min'),
-            wset.get('dis_window_max')
+            wset['dis_win_min'],
+            wset['dis_win_max']
         )
-        iwindow = (
-            wset.get('dis_froz_min'),
-            wset.get('dis_froz_max')
-        )
-        assert(len(filter(None, owindow)) == 2)
-        assert(len(filter(None, iwindow)) == 2)
-    except Exception as e:
-        raise e
+        # ~ iwindow = (
+            # ~ wset['dis_froz_min'],
+            # ~ wset['dis_froz_max']
+        # ~ )
+    except KeyError as e:
+        msg = ('Missing window parameters in input to '
+               'wannier_bands parent calculation:\n') + e.message
+        raise KeyError(msg)
+    except AttributeError as e:
+        msg = ('input wannier_bands must have a WannierCalculation parent!'
+               + e.message)
+        raise AttributeError(msg)
 
     comparison_node = ArrayData()
 
     wbands = wannier_bands.get_bands()
-    vbands = vasp_bands.get_bands()
+    vbands, vocc = vasp_bands.get_bands(also_occupations=True)
 
-    # TODO: get v and w bands into same shape
+    # throw away spin dimension if appropriate
+    if vbands.ndim == 3:
+        vbands = vbands[0]
+        vocc = vocc[0]
 
-    # TODO: grab the vbands within the outer_window
-    vbands_window = '???'
-
-    # TODO: find wich bands within the window match
+    # grab the vbands within the outer_window
+    # find wich bands within the window match
+    # by filling up from the bottom (as w90 seems to do so)
+    vbands_window = np.empty(wbands.shape)
+    vocc_window = np.empty(wbands.shape)
+    count = 0
+    for b in range(vbands.shape[1]):
+        if count < wbands.shape[1]:
+            band = vbands[:, b]
+            if band.min() >= owindow[0] and band.max <= owindow[1]:
+                vbands_window[:, count] = band
+                vocc_window[:, count] = vocc[:, b]
+                count += 1
 
     # TODO: find each band's index (s, px, py, ...)
-
     # TODO: store the legend with the comparison node
 
-    comparison_node.set_array('tb', wbands)
-    comparison_node.set_array('dft', vbands_window)
+    # TODO: find fermi energy from vasp_bands parent
+    if not efermi:
+        efermi = vasp_bands.inp.bands.out.results.get_dict()['efermi']
 
-    return comparison_node
+    comparison_node.set_array('wannier_bands', wbands)
+    comparison_node.set_array('reference', vbands_window)
+
+    return {'bandcmp': comparison_node}
+
+
+def band_gap(bands, occ, efermi, get_pos=False):
+    assert(bands.shape == occ.shape)
+    nbands = bands.shape[1]
+    occupied = [i for i in range(nbands) if occ[:, i].any()]
+    unoccupied = [i for i in range(nbands) if not occ[:, i].any()]
+    # highest band with any occupation
+    homo = bands[:, max(occupied)]
+    # lowest completely unoccupied band
+    lumo = bands[:, min(unoccupied)]
+    gap_lower = homo.max()
+    gap_lower_k = homo.argmax()
+    # if homo crosses efermi, there is no band gap
+    if gap_lower > efermi:
+        return 0, None, []
+    gap_upper = lumo.min()
+    gap_upper_k = lumo.argmin()
+    # if lumo crosses efermi, something is wrong
+    if gap_upper < efermi:
+        raise ValueError(('The given E_fermi was higher than '
+                          'the lowest point of the lowest unoccupied band'))
+    gap = gap_upper - gap_lower
+    direct = gap_upper_k == gap_lower_k
+    # TODO: check wether the two closest points are at the same kpoint (direct)
+    # TODO: or not (indirect)
+    return gap, direct, [gap_lower, gap_upper]
