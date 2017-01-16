756c53ffb9ca2767ad0ccb9de56c274dff51441e
DropD <r.haeuselmann@gmx.ch>
2017-01-16 13:20:54 +0100

    new plugin sys: package name, setup.py, entry pts.

---
+++ README.rst
@@ -0,0 +1,58 @@
+=================
+AiiDA Vasp Plugin
+=================
+
+Install and usage:
+------------------
+
+Install AiiDA
+~~~~~~~~~~~~~
+
+1. Download from [www.aiida.net/?page_id=264](www.aiida.net -> Download) (only EPFL version supports ssh)
+2. install and setup -> [http://aiida-core.readthedocs.org/en/stable/](aiida's documentation)
+
+Install this plugin
+~~~~~~~~~~~~~~~~~~~
+
+From the aiida-vasp folder use::
+
+   $ pip install .
+   # or
+   $ pip install -e . # <-- for plugin developers
+
+To test wether the installation was successful use::
+
+   $ verdi calculation plugins 
+
+   # example output:
+
+   ## Pass as a further parameter one (or more) plugin names
+   ## to get more details on a given plugin.
+   * codtools.cifcellcontents
+   * codtools.cifcodcheck
+   * codtools.cifcodnumbers
+   * codtools.ciffilter
+   * codtools.cifsplitprimitive
+   * quantumespresso.cp
+   * quantumespresso.pw
+   * quantumespresso.pwimmigrant
+   * simpleplugins.templatereplacer
+   * vasp.amn
+   * vasp.scf
+   * vasp.nscf
+   * vasp.vasp5
+   * vasp.wannier
+
+You should see vasp.* in the list
+
+Configure the code
+~~~~~~~~~~~~~~~~~~
+
+See [http://aiida-core.readthedocs.org/en/stable/setup/computerandcodes.html#computer-setup-and-configuration](aiida docs)
+on how to set up computers and codes. Note that you need at least one computer configured and a VASP executable on it
+in order to use this plugin.
+
+Using this plugin
+~~~~~~~~~~~~~~~~~
+
+This section is still in construction
---
+++ setup.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+setup: usage: pip install -e .[graphs]
+"""
+
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    setup(
+        name='aiida_vasp',
+        version='0.9.0a1',
+        description='AiiDA Plugin for running VASP -> Wannier workflows',
+        url='https://github.com/DropD/aiida-vasp',
+        author='Rico Häuselmann',
+        author_email='haeuselm@epfl.ch',
+        license='MIT License, see LICENSE.txt file.',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Plugins',
+            'Framework :: AiiDA',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2.7',
+            'Topic :: Scientific/Engineering :: Physics'
+        ],
+        keywords='vasp aiida wannier90 workflows',
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=[
+            'aiida[ssh]',
+            'ase',
+            'pymatgen'
+        ],
+        extras_require={
+            'graphs': ['matplotlib']
+        },
+        entry_points={
+            'aiida.calculations': [
+                'vasp.scf = aiida_vasp.calcs.scf:ScfCalculation',
+                'vasp.nscf = aiida_vasp.calcs.nscf:NscfCalculation',
+                'vasp.amn = aiida_vasp.calcs.amn:AmnCalculation',
+                'vasp.wannier = aiida_vasp.calcs.wannier:WannierCalculation',
+                'vasp.vasp5 = aiida_vasp.calcs.vasp5:Vasp5Calculation'
+            ],
+            'aiida.data': [
+                'vasp.archive = aiida_vasp.data.archive:ArchiveData',
+                'vasp.chargedensity = aiida_vasp.data.chargedensity:ChargedensityData',
+                'vasp.paw = aiida_vasp.data.paw:PawData',
+                'vasp.wavefun = aiida_vasp.data.wavefun:WavefunData'
+            ],
+            'aiida.parsers': [
+                'vasp.scf = aiida_vasp.parsers.scf:ScfParser',
+                'vasp.nscf = aiida_vasp.parsers.nscf:NscfParser',
+                'vasp.amn = aiida_vasp.parsers.amn:AmnParsaer',
+                'vasp.wannier = aiida_vasp.parsers.wannier:WannierParser',
+                'vasp.vasp5 = aiida_vasp.parsers.vasp5:Vasp5Parser'
+            ],
+            'aiida.workflows': [
+                'vasp.scf = aiida_vasp.workflows.scf:ScfWorkflow',
+                'vasp.nscf = aiida_vasp.workflows.nscf:NscfWorkflow',
+                'vasp.projections = aiida_vasp.workflows.projections:ProjectionsWorkflow',
+                'vasp.autowindows = aiida_vasp.workflows.autowindows:AutowindowsWorkflow'
+            ],
+            'aiida.cmdline': [
+                'vasp.paw = aiida_vasp.commands.paw:_Paw'
+            ]
+        },
+        scripts=['utils/runwf.py']
+    )
