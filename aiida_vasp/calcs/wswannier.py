ebdf53ea9d050ed2059b7ac4b6daed856dfb4c59
Dominik Gresch <greschd@gmx.ch>
2017-06-12 09:53:56 +0200

    Change absolute -> relative import of vasp.wannier im wswannier

--- aiida_vasp/calcs/wswannier.py
+++ aiida_vasp/calcs/wswannier.py
@@ -1,4 +1,4 @@
-from aiida.orm.calculation.job.vasp.wannier import WannierCalculation
+from .wannier import WannierCalculation
 
 class WswannierCalculation(WannierCalculation):
     default_parser = 'vasp.wswannier'
