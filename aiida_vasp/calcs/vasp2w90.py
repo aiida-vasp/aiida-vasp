1731d23413828ea467f4138e906399b6a699c854
Dominik Gresch <greschd@gmx.ch>
2017-07-31 13:56:18 +0200

    Remove unused 'dict_to_win' function

--- aiida_vasp/calcs/vasp2w90.py
+++ aiida_vasp/calcs/vasp2w90.py
@@ -4,61 +4,6 @@ from aiida.orm import DataFactory
 from aiida.orm.data.base import List
 from aiida_wannier90.io import write_win
 
-
-class dict_to_win(object):
-
-    @classmethod
-    def _bool(cls, val):
-        return val and 'T' or 'F'
-
-    @classmethod
-    def _seq(cls, val):
-        res = []
-        for i in val:
-            if not isinstance(i, (list, tuple)):
-                line = cls._value(i)
-            else:
-                line = ' '.join(map(cls._value, i))
-            res.append(' ' + line)
-        return res
-
-    @classmethod
-    def _block(cls, name, val):
-        res = ['begin '+name]
-        res += cls._value(val)
-        res += ['end '+name]
-        return res
-
-    @classmethod
-    def _assign(cls, key, val):
-        return '{} = {}'.format(key, val)
-
-    @classmethod
-    def _value(cls, val):
-        if isinstance(val, (str, unicode)):
-            return val
-        elif isinstance(val, bool):
-            return cls._bool(val)
-        elif isinstance(val, (list, tuple)):
-            return cls._seq(val)
-        else:
-            return str(val)
-
-    @classmethod
-    def _item(cls, key, val):
-        if isinstance(val, (list, tuple)):
-            return cls._block(key, val)
-        else:
-            return [cls._assign(key, cls._value(val))]
-
-    @classmethod
-    def parse(cls, in_dict):
-        res = []
-        for k, v in in_dict.iteritems():
-            res += cls._item(k, v)
-        return '\n'.join(res)
-
-
 class Vasp2w90Calculation(VaspCalculation):
     '''General purpose Calculation for using vasp with the vasp2wannier90 interface.'''
 
