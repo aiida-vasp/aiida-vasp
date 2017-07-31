bb4cffc7a39adf17d6e19dd603fed26f877d0820
Dominik Gresch <greschd@gmx.ch>
2017-07-31 09:42:41 +0200

    Make parser rate run as 'unsuccessful' if the Wannier data doesn't exist

--- tests_pytest/test_vasp2w90.py
+++ tests_pytest/test_vasp2w90.py
@@ -1,13 +1,14 @@
 from gaas_sample import get_gaas_process_inputs
 
 
-def test_vasp5(configure_with_daemon, get_gaas_process_inputs, assert_finished):
+def test_vasp2w90(configure_with_daemon, get_gaas_process_inputs, assert_finished):
     from aiida.work.run import run
 
-    process, inputs = get_gaas_process_inputs(calculation_string='vasp.vasp5')
+    process, inputs = get_gaas_process_inputs(calculation_string='vasp.vasp2w90')
     output, pid = run(
         process,
         _return_pid=True,
         **inputs
     )
+    print(output)
     assert_finished(pid)
