1ac5ed8d59a6a666bc9db242226424a3dd89bd69
Dominik Gresch <greschd@gmx.ch>
2017-09-21 15:23:47 +0200

    Run YAPF and fix pylint issues

--- tests_pytest/test_win_parser.py
+++ tests_pytest/test_win_parser.py
@@ -5,20 +5,29 @@ import pytest
 
 from aiida_vasp.utils.io.win import WinParser
 
+
 @pytest.fixture(params=['0.win', '1.win'])
 def win_result(request, sample):
     filename = request.param
     win_parser = WinParser(sample(os.path.join('win_files', filename)))
     return win_parser.result
 
-def test_keys(win_result):
+
+def test_keys(win_result):  # pylint: disable=redefined-outer-name
     assert set(win_result.keys()) == set([
-        'projections', 'spinors', 'unit_cell_cart', 'atoms_cart', 'mp_grid',
-        'kpoints', 'num_wann',
+        'projections',
+        'spinors',
+        'unit_cell_cart',
+        'atoms_cart',
+        'mp_grid',
+        'kpoints',
+        'num_wann',
     ])
 
-def test_no_empty_key(win_result):
+
+def test_no_empty_key(win_result):  # pylint: disable=redefined-outer-name
     assert '' not in win_result.keys()
 
-def test_projections(win_result):
+
+def test_projections(win_result):  # pylint: disable=redefined-outer-name
     assert all(isinstance(line, str) for line in win_result['projections'])
