481ad80768318df2e8977b4c564af3dd286b5b2f
DropD <r.haeuselmann@gmx.ch>
2017-09-22 12:41:50 +0200

    use fixture from github.com/DropD/aiida-core/tree/plugin_fixtures

--- run_ci_tests.sh
+++ run_ci_tests.sh
@@ -2,7 +2,7 @@ set -e
 case "$TEST_TYPE" in 
     unittests)
         set -x
-        pytest --cov-report=term-missing --cov=aiida_vasp --ignore=tests_pytest/ --ignore=tests_pytest/conftest.py
+        pytest --cov-report=term-missing --cov=aiida_vasp
         ;;
     pre-commit)
         set -x
