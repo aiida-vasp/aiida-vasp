639432c487ebfbae88e486eaff7940203d2a39cb
DropD <r.haeuselmann@gmx.ch>
2017-09-22 11:48:19 +0200

    ensure tets script fails at first error

--- run_ci_tests.sh
+++ run_ci_tests.sh
@@ -1,13 +1,12 @@
+set -e
 case "$TEST_TYPE" in 
     unittests)
         set -x
         pytest --cov-report=term-missing --cov=aiida_vasp --ignore=tests_pytest/
-        set +x
         ;;
     pre-commit)
         set -x
         pre-commit run --all-files
-        set +x
         ;;
     *)
         echo "Invalid value for TEST_TYPE env variable."
