d3ba7947162eee07d17fedd0b6601f68f741ce7e
DropD <r.haeuselmann@gmx.ch>
2017-10-24 13:46:14 +0200

    fix setup.json location in setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@
 setup: usage: pip install -e .[graphs]
 """
 
+import os
 import json
 from setuptools import setup, find_packages
 
+SETUP_JSON_PATH = os.path.join(os.path.dirname(__file__), 'setup.json')
+
 if __name__ == '__main__':
-    with open('setup.json', 'r') as info:
+    with open(SETUP_JSON_PATH, 'r') as info:
         SETUP_KWARGS = json.load(info)
     setup(packages=find_packages(exclude=['aiida']), **SETUP_KWARGS)
