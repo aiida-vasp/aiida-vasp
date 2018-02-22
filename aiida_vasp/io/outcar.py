1020508038e5292056da2ca07947a1a88d3b784b
Martin Callsen <martin.callsen@ugent.be>
2018-02-22 11:19:13 +0100

    Refactored the temporary OUTCAR parser to parse OUTCAR only once.

--- aiida_vasp/io/outcar.py
+++ aiida_vasp/io/outcar.py
@@ -11,56 +11,49 @@ class OutcarParser(object):
     def __init__(self, fname):
         super(OutcarParser, self).__init__()
         self.outcar_file = fname
-        self.properties = ['volume', 'energies', 'efermi']
+        self.properties = ['volume', 'free_energy', 'free_energy_all', 'energy_without_entropy', 'energy_without_entropy_all', 'efermi']
+        self._cached_data = {}
 
     @property
     def output_dict(self):
         """Parse the OUTCAR file and return the parsed values wrapped in a dictionary"""
         output_dict = {}
         for property_name in self.properties:
-            try:
-                # call the method corresponding to property_name.
-                result = getattr(self, '_read_' + property_name)()
-                if result is not None:
-                    # read_property_name returns a dictionary if parsing has been successful.
-                    output_dict.update(result)
-            except AttributeError:
-                raise NotImplementedError('The OUTCAR parser does not implemnt _read_{}'.format(property_name))
+            # _get_quantity returns a dictionary containing the property if parsing has been successful.
+            output_dict.update(self._get_quantity(property_name))
         return output_dict
 
-    def _read_volume(self):
-        """Parse the OUTCAR file and return the cell volume"""
+    def _parse_outcar(self):
+        """Parse the OUTCAR file into a dictionary."""
         result = {}
-        with open(self.outcar_file, 'r') as outcar_file_object:
-            for line in outcar_file_object:
-                if line.rfind('volume of cell :') > -1:
-                    result['volume'] = float(line.split()[-1])
-        return result
-
-    def _read_energies(self):
-        """Parse the OUTCAR file and return the total energies without entropy as well as free energies"""
         energy_free = []
         energy_zero = []
         with open(self.outcar_file, 'r') as outcar_file_object:
             for line in outcar_file_object:
+                # volume
+                if line.rfind('volume of cell :') > -1:
+                    result['volume'] = float(line.split()[-1])
                 # Free energy
                 if line.lower().startswith('  free  energy   toten'):
                     energy_free.append(float(line.split()[-2]))
                 # Extrapolated zero point energy
                 if line.startswith('  energy  without entropy'):
                     energy_zero.append(float(line.split()[-1]))
-        result = {}
+                # Fermi energy
+                if line.rfind('E-fermi') > -1:
+                    result['efermi'] = float(line.split()[2])
         result['free_energy'] = energy_free[-1]
         result['energy_without_entropy'] = energy_zero[-1]
         result['free_energy_all'] = energy_free
         result['energy_without_entropy_all'] = energy_zero
         return result
 
-    def _read_efermi(self):
-        """Parse the OUTCAR file and return the fermi energy"""
-        result = {}
-        with open(self.outcar_file, 'r') as outcar_file_object:
-            for line in outcar_file_object:
-                if line.rfind('E-fermi') > -1:
-                    result['efermi'] = float(line.split()[2])
-        return result
+    def _get_quantity(self, quantity):
+        """Return the requested quantity from the _cached_data. If OUTCAR has not been parsed yet, parse it."""
+        if not self._cached_data:
+            self._cached_data = self._parse_outcar()
+
+        if quantity not in self._cached_data:
+            return {}
+
+        return {quantity: self._cached_data[quantity]}
