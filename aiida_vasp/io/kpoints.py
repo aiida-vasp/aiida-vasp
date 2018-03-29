00177d495eecc90eb9c4c931fbefb1caecba00ec
Martin Callsen <martin.callsen@ugent.be>
2018-03-29 11:26:49 +0200

    added 'kpoints_raw' to parsable items list for the KpParser unit tests

--- aiida_vasp/io/kpoints.py
+++ aiida_vasp/io/kpoints.py
@@ -15,6 +15,12 @@ class KpParser(BaseFileParser):
             'nodeName': 'kpoints',
             'prerequisites': []
         },
+        'kpoints_raw': {
+            'inputs': [],
+            'parsers': ['IBZKPT'],
+            'nodeName': 'kpoints',
+            'prerequisites': []
+        },
     }
 
     def __init__(self, path, filename):
