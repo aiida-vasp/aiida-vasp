4c6d939f090d14bbe6650cd2bac4ac89cc2b44dc
Martin Callsen <martin.callsen@ugent.be>
2018-04-03 13:40:26 +0200

    Tests for reading KPOINTS files have been migrated to io.tests.test_kpoints_io

--- aiida_vasp/backendtests/basic.py
+++ aiida_vasp/backendtests/basic.py
@@ -99,31 +99,6 @@ class BasicCalcTest(AiidaTestCase):
         os.chdir(os.path.dirname(working_dir))
         self.assertIsNotNone(poscar)
 
-    def test_write_kpoints_mesh(self):
-        """Check that KPOINTS in mesh format is written correctly"""
-        from aiida_vasp.io.kpoints import KpParser
-        calc = self._get_calc('c', 'm')
-        inp = calc.get_inputs_dict()
-        calc.write_kpoints(inp, self.tmpf)
-        kpp = KpParser(self.tmpf, 'KPOINTS')
-        result = {}
-        kpp.get_quantity('kpoints_raw', result)
-        self.assertTrue((result['kpoints_raw'] == Common.kpoints_mesh_res()).all())
-
-    def test_write_kpoints_list(self):
-        """Check that KPOINTS in list format is written correctly"""
-        from aiida_vasp.io.kpoints import KpParser
-        calc = self._get_calc('c', 'l')
-        inp = calc.get_inputs_dict()
-        calc.write_kpoints(inp, self.tmpf)
-        kpp = KpParser(self.tmpf, 'KPOINTS')
-        kres, wres = Common.kpoints_list_res()
-        result = {}
-        kpp.get_quantity('kpoints_raw', result)
-        kpp.get_quantity('weights', result)
-        self.assertTrue((result['kpoints_raw'] == kres).all())
-        self.assertTrue((result['weights'] == wres).all())
-
     def test_write_potcar(self):
         """Check that POTCAR is written correctly"""
         calc = self._get_calc('c', 'm')
