fced5cee4199fe08795b5df697a61627001a1612
DropD <r.haeuselmann@gmx.ch>
2018-08-07 15:06:15 +0200

    immigrant: split create_input_nodes, add settings input

--- aiida_vasp/calcs/immigrant.py
+++ aiida_vasp/calcs/immigrant.py
@@ -5,11 +5,18 @@ import os
 
 from py import path as py_path  # pylint: disable=no-name-in-module,no-member
 from aiida.common.folders import SandboxFolder
+from aiida.common.exceptions import InputValidationError
 
 from aiida_vasp.calcs.vasp import VaspCalculation
-from aiida_vasp.io.incar import IncarIo
+from aiida_vasp.io.incar import IncarParser
 from aiida_vasp.io.kpoints import KpParser
 from aiida_vasp.io.poscar import PoscarParser
+from aiida_vasp.io.potcar import MultiPotcarIo
+from aiida_vasp.utils.aiida_utils import get_data_node
+
+
+def get_quantity_node(parser, quantity):
+    return parser.get_quantit(quantity, None)[quantity]
 
 
 ## TODO: add a test
@@ -22,14 +29,56 @@ class VaspImmigrant(VaspCalculation):
         * settings has to be given manually
     """
 
-    def _process_remote_workdir(remote_workdir):
+    def _process_remote_workdir(self, remote_workdir):
         if remote_workdir:
             self.set_remote_workdir(remote_workdir)
         elif not self._get_attr('remote_workdir', None):
-            raise InputValidationEror(
-                'expeced keyword parameter ``remote_workdir`` as it has not been set previously with ``.set_remote_workdir()``')
+            raise InputValidationError(
+                'expected keyword parameter ``remote_workdir`` as it has not been set previously with ``.set_remote_workdir()``')
 
-    def create_input_nodes(self, open_transport, remote_workdir=None):
+    def _create_incar_input(self, open_transport, sandbox_path):
+        """Copy the INCAR file, create a parameter node from it and use that."""
+        remote_path = os.path.join(self._get_remote_workdir(), self._INPUT_FILE_NAME)
+        open_transport.get(remote_path, sandbox_path.strpath)
+        local_incar = sandbox_path.join(self._INPUT_FILE_NAME)
+        incar_in = IncarParser(file_path=local_incar.strpath)
+        incar = get_quantity_node(incar_in, 'incar')
+        self.use_parameters(incar)
+        return incar
+
+    def _create_poscar_input(self, open_transport, sandbox_path):
+        """Copy the POSCAR file, create a structure node from it and use that."""
+        ## TODO: allow user input for symbols / kind names
+        remote_path = os.path.join(self._get_remote_workdir(), 'POSCAR')
+        open_transport.get(remote_path, sandbox_path.strpath)
+        local_poscar = sandbox_path.join('POSCAR')
+        poscar_in = PoscarParser(file_path=local_poscar.strpath)
+        structure = get_quantity_node(poscar_in, 'poscar-poscar')
+        self.use_structure(structure)
+        return structure
+
+    def _create_potcar_input(self, open_transport, sandbox_path, structure):
+        """Copy the POTCAR files, retrieve the potcar nodes for them and use those."""
+        remote_path = os.path.join(self._get_remote_workdir(), 'POTCAR')
+        open_transport.get(remote_path, sandbox_path.strpath)
+        local_potcar = sandbox_path.join('POTCAR')
+        multi_potcar_io = MultiPotcarIo.read(local_potcar.strpath)
+        for kind_name, potcar in multi_potcar_io.get_potentials_dict(structure).items():
+            self.use_potential(potcar, kind=kind_name)
+        return multi_potcar_io
+
+    def _create_kpoints_input(self, open_transport, sandbox_path, structure):
+        """Copy the KPOINTS file, make it into a kpoints node and use that."""
+        remote_path = os.path.join(self._get_remote_workdir(), 'KPOINTS')
+        open_transport.get(remote_path, sandbox_path.strpath)
+        local_kpoints = sandbox_path.join('KPOINTS')
+        kpoints_in = KpParser(file_path=local_kpoints.strpath)
+        kpoints = get_quantity_node(kpoints_in, 'kpoints-kpoints')
+        kpoints.set_cell_from_structure(structure)
+        self.use_kpoints(kpoints)
+        return kpoints
+
+    def create_input_nodes(self, open_transport, remote_workdir=None, settings_dict=None):
         """
         Create a Calculation based on a folder in which VASP was run.
 
@@ -37,41 +86,20 @@ class VaspImmigrant(VaspCalculation):
         :param remote_workdir: optional only if ``remote_workdir`` has already been set previously using ``set_remote_workdir()``
                 Absolute path to the directory where the calculation was run. All input and output files are expected to be found
                 at the given path by the given transport instance.
+        :param settings_dict: An optional dictionary with a dictionary for the ``settings`` input.
         """
         self._process_remote_workdir(remote_workdir)
         # Copy the input file and psuedo files to a temp folder for parsing.
         with SandboxFolder() as sandbox:
 
             sandbox_path = py_path.local(sandbox.abspath)
-            ## Copy the input file to the temp folder.
-            remote_path = os.path.join(self._get_remote_workdir(), self._INPUT_FILE_NAME)
-            open_transport.get(remote_path, folder.abspath)
 
-            local_incar = sandbox_path.join(self._INPUT_FILE_NAME)
-            incar_io = IncarIo(file_path=local_incar.strpath)
-            self.use_parameters(incar_io.get_param_node())
+            _ = self._create_incar_input(open_transport, sandbox_path)
+            structure = self._create_poscar_input(open_transport, sandbox_path)
+            _ = self._create_potcar_input(open_transport, sandbox_path, structure)
+            _ = self._create_kpoints_input(open_transport, sandbox_path, structure)
 
-            ## Create the input structure from POSCAR
-            ## TODO: allow user input for symbols / kind names
-            remote_path = os.path.join(self._get_remote_workdir(), 'POSCAR')
-            local_poscar = sandbox_path.join('POSCAR')
-            poscar_in = PoscarParser(file_path=local_poscar.strpath)
-            structure = poscar_in.get_quantity('poscar', None)['structure']
-            self.use_structure(structure)
+        if settings_dict:
+            self.use_settings(get_data_node('parameter', dict=settings_dict))
 
-            ## Do the same with KPOINTS
-            remote_path = os.path.join(self._get_remote_workdir(), 'KPOINTS')
-            open_transport.get(remote_path, folder.abspath)
-            local_kpoints = sandbox_path.join('KPOINTS')
-            kpoints_in = KpParser(file_path=local_kpoints.strpath)
-            kpoints = kpoints_in.get_quantity('kpoints', None)['kpoints']
-            kpoints.set_cell_from_structure(structure)
-            self.use_kpoints(kpoints)
-
-            ## Now get the PotcarData associated with the used POTCARs
-            remote_path = os.path.join(self._get_remote_workdir(), 'POTCAR')
-            open_transport.get(remote_path, folder.abspath)
-            local_potcar = sandbox_path.join('POTCAR')
-            multi_potcar_io = MultiPotcarIo.read(local_potcar.strpath)
-            for kind_name, potcar in multi_potcar_io.get_potentials_dict().items():
-                self.use_potential(potcar, kind=kind_name)
+        self._set_attr('input_nodes_created', True)
