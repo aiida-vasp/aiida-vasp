1894c6e82ff0303d4250cb23f5c97964ba3257ec
DropD <r.haeuselmann@gmx.ch>
2018-08-20 13:18:33 +0200

    immigrant: add test for raw_input folder

--- aiida_vasp/calcs/tests/test_immigrant.py
+++ aiida_vasp/calcs/tests/test_immigrant.py
@@ -23,14 +23,30 @@ def immigrant_with_builder(fresh_aiida_env, potcar_family, phonondb_run, localho
 @pytest.mark.skipif(aiida_version() < cmp_version('1.0.0a1'), reason='too many JobProcess changes')
 def test_immigrant_additional(fresh_aiida_env, potcar_family, phonondb_run, localhost, mock_vasp):
     """Provide process class and inputs for importing a AiiDA-external VASP run."""
+    from aiida import work
     from aiida_vasp.calcs.vasp import VaspCalculation
     create_authinfo(localhost, store=True)
     potcar_spec = {'family': POTCAR_FAMILY_NAME, 'map': POTCAR_MAP}
-    _, builder = VaspCalculation.immigrant(
+    proc, builder = VaspCalculation.immigrant(
         code=mock_vasp, remote_path=phonondb_run, potcar_spec=potcar_spec, use_chgcar=True, use_wavecar=True)
     expected_inputs = {'parameters', 'structure', 'kpoints', 'potential', 'charge_density', 'wavefunctions'}
     for input_link in expected_inputs:
         assert builder.get(input_link, None) is not None, 'input link "{}" was not set!'.format(input_link)
+
+    runner = work.runners.new_runner()
+    result = runner.run(proc, **builder)
+
+    expected_files = {'INCAR', 'POSCAR', 'KPOINTS', 'CHGCAR', 'WAVECAR'}
+    banned_files = {'POTCAR'}
+
+    calc = result['retrieved'].get_inputs()[0]
+
+    assert 'raw_input' in calc.folder.get_content_list()
+    input_folder = calc.folder.get_subfolder('raw_input')
+
+    input_files = set(input_folder.get_content_list())
+    assert expected_files.issubset(input_files)
+    assert banned_files.isdisjoint(input_files)
 
 
 @pytest.mark.skipif(aiida_version() < cmp_version('1.0.0a1'), reason='too many JobProcess changes')
