64ec78426fe1f5b2c39eb6c7cfa42e298410e7bc
Espen <espenfl@online.no>
2018-09-27 12:39:53 +0200

    Added parser quantities to output_parameters (#219)
    * Added parser quantities to output_parameters
    
    * Minor correction to naming scheme

--- aiida_vasp/io/tests/test_vasprun_io.py
+++ aiida_vasp/io/tests/test_vasprun_io.py
@@ -30,13 +30,16 @@ def test_parameter_results(vasprun_parser):
 
     """
 
-    vasprun_parser.settings.update_with({'output_params': ['fermi_level']})
+    vasprun_parser.settings.update_with({'output_params': ['fermi_level', 'total_energies', 'maximum_force', 'maximum_stress']})
     quantity = vasprun_parser.get_quantity('parameters')
     data_obj = quantity['parameters']
     ref_class = get_data_class('parameter')
     assert isinstance(data_obj, ref_class)
     data_dict = data_obj.get_dict()
     assert data_dict['fermi_level'] == 5.96764939
+    assert data_dict['total_energies']['energy_no_entropy'] == -42.91113621
+    assert data_dict['maximum_stress'] == 28.803993008871014
+    assert data_dict['maximum_force'] == 3.41460162
 
 
 @pytest.mark.parametrize(['vasprun_parser'], [('basic',)], indirect=True)
