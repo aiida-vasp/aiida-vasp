1d5a37b3854818b0aa92dd317ab475d403f71d8c
Jonathan Chico <jonathan.chico@sandvik.com>
2020-11-04 14:41:18 +0100

    Fixing error that occurred during merging

--- aiida_vasp/utils/fixtures/__init__.py
+++ aiida_vasp/utils/fixtures/__init__.py
@@ -1,12 +1,8 @@
 """Expose all fixtures."""
 from .data import localhost_dir, localhost, vasp_params, potentials, vasp_structure, vasp_kpoints, vasp_code, ref_incar, \
     ref_incar_vasp2w90, vasp_chgcar, vasp_wavecar, ref_retrieved, vasp_structure_poscar, temp_pot_folder, potcar_family, \
-<<<<<<< HEAD
     vasprun_parser, outcar_parser, mock_vasp, wannier_params, wannier_projections, ref_win, phonondb_run, vasp_inputs, \
     vasp2w90_inputs, stream_parser
-=======
-    vasprun_parser, outcar_parser, mock_vasp, wannier_params, wannier_projections, ref_win, phonondb_run, vasp_inputs, vasp2w90_inputs
->>>>>>> 90626f5... Changed the magnetization tests so that they use only a single parsing function
 from .environment import fresh_aiida_env
 from .calcs import base_calc, vasp_calc_and_ref, vasp2w90_calc_and_ref, vasp2w90_calc, vasp_calc, run_vasp_calc
 
