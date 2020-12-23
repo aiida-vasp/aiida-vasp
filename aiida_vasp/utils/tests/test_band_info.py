2e5d699b786d5408c46770c9aa38d69b7418e430
Bonan Zhu <zhubonan@outlook.com>
2020-12-23 14:01:27 +0000

    Fix for tests

--- aiida_vasp/utils/tests/test_band_info.py
+++ aiida_vasp/utils/tests/test_band_info.py
@@ -4,7 +4,7 @@ import pytest
 import numpy as np
 
 from aiida_vasp.utils.fixtures.environment import fresh_aiida_env
-from aiida_vasp.utils.compare_bands import get_bands_info_from_bands_data, get_bands_info
+from aiida_vasp.utils.compare_bands import get_band_properties_from_data, get_band_properties
 
 
 @pytest.fixture
@@ -35,24 +35,24 @@ def test_bands_info():
     """Test getting bands info"""
     occ = np.array([[1, 1, 1, 0, 0, 0]])
     eigen = np.array([[-1, -1, -1, 0, 0, 0]])
-    bands_info = get_bands_info(eigen, occ)
+    bands_info = get_band_properties(eigen, occ)
     assert bands_info['is_direct_gap'] is True
     assert bands_info['band_gap'] == 1.0
 
     occ = np.array([[1, 1, 1, 0, 0, 0], [1, 1, 1, 0, 0, 0]])
     eigen = np.array([[-1, -1, -1, 0, 0, 0], [-0.5, -0.5, -0.5, 0, 0, 0]])
-    bands_info = get_bands_info(eigen, occ)
+    bands_info = get_band_properties(eigen, occ)
     assert bands_info['is_direct_gap'] is False
     assert bands_info['band_gap'] == 0.5
 
 
 def test_bands_info_from_data(example_bands, example_bands_v2):
     """Test getting info from BandsData"""
-    bands_info = get_bands_info_from_bands_data(example_bands)
+    bands_info = get_band_properties_from_data(example_bands)
     assert bands_info['is_direct_gap'] is True
     assert bands_info['band_gap'] == 1.0
 
-    bands_info = get_bands_info_from_bands_data(example_bands_v2)
+    bands_info = get_band_properties_from_data(example_bands_v2)
     assert bands_info['is_direct_gap'] is False
     assert bands_info['band_gap'] == 0.5
     assert bands_info['vbm'] == -0.5
