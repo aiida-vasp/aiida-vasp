60224774c26353d4016db9dabe32b783c10dc0a9
Bonan Zhu <zhubonan@outlook.com>
2021-01-22 12:39:51 +0000

    Bug fix and added test cases

--- aiida_vasp/parsers/file_parsers/vasprun.py
+++ aiida_vasp/parsers/file_parsers/vasprun.py
@@ -679,15 +679,26 @@ class VasprunParser(BaseFileParser):
         parameters = self._xml.get_parameters()
         info['vasp_finished'] = not self._xml_truncated
         # Only set to true for untruncated run to avoid false positives
-        if len(nosc_energies) < parameters['nelm'] and not self._xml_truncated:
+        if nosc_energies is None:
+            info['electronic_converged'] = False
+        elif len(nosc_energies) < parameters['nelm'] and not self._xml_truncated:
             info['electronic_converged'] = True
         else:
             info['electronic_converged'] = False
+
         all_energies = self._xml.get_energies('all', nosc=False)
-        if len(all_energies) < parameters['nsw'] and not self._xml_truncated:
-            info['ionic_converged'] = True
-        else:
+        if all_energies is None:
             info['ionic_converged'] = False
+        else:
+            sc_steps = all_energies.get('electronic_steps')
+            if len(sc_steps) <= parameters['nsw'] and not self._xml_truncated:
+                info['ionic_converged'] = True
+            else:
+                info['ionic_converged'] = False
+        # Override if nsw is 0 - no ionic steps are performed
+        if parameters['nsw'] < 1:
+            info['ionic_converged'] = None
+
         return info
 
 
