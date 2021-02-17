bee3919b1a3cb3a16f819b40633dceb98126f65c
espenfl <espen.flage-larsen@sintef.no>
2021-02-17 22:16:08 +0100

    Pass label set at WorkChain to CalcJob

--- aiida_vasp/workchains/vasp.py
+++ aiida_vasp/workchains/vasp.py
@@ -161,7 +161,7 @@ class VaspWorkChain(BaseRestartWorkChain):
             if parameters != old_parameters:
                 self.ctx.inputs.parameters = get_data_node('dict', dict=parameters)
 
-    def init_inputs(self):
+    def init_inputs(self):  # pylint: disable=too-many-branches, too-many-statements
         """Make sure all the required inputs are there and valid, create input dictionary for calculation."""
         self.ctx.inputs = AttributeDict()
         self.ctx.inputs.parameters = self._init_parameters()
@@ -218,6 +218,14 @@ class VaspWorkChain(BaseRestartWorkChain):
             # Set MPI to True, unless the user specifies otherwise
             withmpi = self.ctx.inputs.metadata['options'].get('withmpi', True)
             self.ctx.inputs.metadata['options']['withmpi'] = withmpi
+
+        # Make sure we also bring along any label set on the WorkChain to the CalcJob
+        if 'metadata' in self.inputs:
+            if 'label' in self.inputs.metadata:
+                if 'metadata' in self.ctx.inputs:
+                    self.ctx.inputs.metadata['label'] = self.inputs.metadata.label
+                else:
+                    self.ctx.inputs.metadata = {'label': self.inputs.metadata.label}
 
         # Verify and set potentials (potcar)
         if not self.inputs.potential_family.value:
