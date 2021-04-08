a612ab8a818ac7106917879fe9d8f118fc9af9fa
Bonan Zhu <zhubonan@outlook.com>
2021-04-08 14:26:37 +0100

    Properly remove the comment section

--- aiida_vasp/utils/mock_code.py
+++ aiida_vasp/utils/mock_code.py
@@ -112,7 +112,7 @@ class MockRegistry:
         if kpt_file.is_file():
             kpoints = Kpoints(file_path=str(kpt_file))
             items['kpoints'] = kpoints.get_dict()
-            items.pop('comment', None)
+            items['kpoints'].pop('comment', None)
 
         incar_file = input_folder / 'INCAR'
         if incar_file.is_file():
@@ -123,7 +123,7 @@ class MockRegistry:
         if poscar_file.is_file():
             poscar = Poscar(file_path=str(poscar_file))
             items['poscar'] = poscar.get_dict()
-            items.pop('comment', None)
+            items['poscar'].pop('comment', None)
 
         return get_hash(items)[0]
 
