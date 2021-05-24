c2deb86ad9868acd9dbe04910be629e3f1132d50
Bonan Zhu <zhubonan@outlook.com>
2021-05-24 12:05:37 +0100

    Replace np.all with np.allclose

--- aiida_vasp/parsers/file_parsers/tests/test_vasprun_parser.py
+++ aiida_vasp/parsers/file_parsers/tests/test_vasprun_parser.py
@@ -66,8 +66,8 @@ def test_kpoints(fresh_aiida_env, vasprun_parser):
 
     ref_class = get_data_class('array.kpoints')
     assert isinstance(data_obj, ref_class)
-    assert np.all(data_obj.get_kpoints()[0] == np.array([0.0, 0.0, 0.0]))
-    assert np.all(data_obj.get_kpoints()[-1] == np.array([0.42857143, -0.42857143, 0.42857143]))
+    assert np.allclose(data_obj.get_kpoints()[0], np.array([0.0, 0.0, 0.0]))
+    assert np.allclose(data_obj.get_kpoints()[-1], np.array([0.42857143, -0.42857143, 0.42857143]))
 
 
 @pytest.mark.parametrize('vasprun_parser', [('basic', {})], indirect=True)
@@ -86,12 +86,12 @@ def test_structure(fresh_aiida_env, vasprun_parser):
     assert isinstance(data_obj, ref_obj)
     # check cell
     unitcell = data_obj.cell
-    assert np.all(unitcell[0] == np.array([5.46503124, 0.0, 0.0]))
-    assert np.all(unitcell[1] == np.array([0.0, 5.46503124, 0.0]))
-    assert np.all(unitcell[2] == np.array([0.0, 0.0, 5.46503124]))
+    assert np.allclose(unitcell[0], np.array([5.46503124, 0.0, 0.0]))
+    assert np.allclose(unitcell[1], np.array([0.0, 5.46503124, 0.0]))
+    assert np.allclose(unitcell[2], np.array([0.0, 0.0, 5.46503124]))
     # check first and last position
-    assert np.all(data_obj.sites[0].position == np.array([0.0, 0.0, 0.0]))
-    assert np.all(data_obj.sites[7].position == np.array([4.09877343, 4.09877343, 1.36625781]))
+    assert np.allclose(data_obj.sites[0].position, np.array([0.0, 0.0, 0.0]))
+    assert np.allclose(data_obj.sites[7].position, np.array([4.09877343, 4.09877343, 1.36625781]))
     # check volume
     assert data_obj.get_cell_volume() == np.float(163.22171870360754)
 
@@ -111,9 +111,9 @@ def test_final_force(fresh_aiida_env, vasprun_parser):
 
     forces = data_obj.get_array('final')
     # check first, third and last position
-    assert np.all(forces[0] == forces_check[0])
-    assert np.all(forces[2] == forces_check[2])
-    assert np.all(forces[7] == forces_check[7])
+    assert np.allclose(forces[0], forces_check[0])
+    assert np.allclose(forces[2], forces_check[2])
+    assert np.allclose(forces[7], forces_check[7])
 
 
 @pytest.mark.parametrize('vasprun_parser', [('basic', {})], indirect=True)
@@ -129,9 +129,9 @@ def test_final_stress(fresh_aiida_env, vasprun_parser):
                              [0.00000000, -25.93894358, 12.52362644]])
     stress = data_obj.get_array('final')
     # check entries
-    assert np.all(stress[0] == stress_check[0])
-    assert np.all(stress[1] == stress_check[1])
-    assert np.all(stress[2] == stress_check[2])
+    assert np.allclose(stress[0], stress_check[0])
+    assert np.allclose(stress[1], stress_check[1])
+    assert np.allclose(stress[2], stress_check[2])
 
 
 @pytest.mark.parametrize('vasprun_parser', [('basic', {})], indirect=True)
@@ -152,17 +152,17 @@ def test_traj_forces(fresh_aiida_env, vasprun_parser):
     assert isinstance(data_obj, ref_obj)
     data_obj_arr = data_obj.get_array('forces')
     # test entries
-    assert np.all(data_obj_arr[0][0] == np.array([-0.24286901, 0.0, 0.0]))
-    assert np.all(data_obj_arr[0][-1] == np.array([-0.73887169, -0.43727184, -0.43727184]))
+    assert np.allclose(data_obj_arr[0][0], np.array([-0.24286901, 0.0, 0.0]))
+    assert np.allclose(data_obj_arr[0][-1], np.array([-0.73887169, -0.43727184, -0.43727184]))
     # test object
     ref_obj = get_data_class('array.trajectory')
     assert isinstance(data_obj, ref_obj)
     data_obj = data_obj.get_array('forces')
     # test entries
-    assert np.all(data_obj[0][0] == np.array([-0.24286901, 0.0, 0.0]))
-    assert np.all(data_obj[0][-1] == np.array([-0.73887169, -0.43727184, -0.43727184]))
-    assert np.all(data_obj[0][-1] == data_obj[1][-1])
-    assert np.all(data_obj[0][0] == data_obj[1][0])
+    assert np.allclose(data_obj[0][0], np.array([-0.24286901, 0.0, 0.0]))
+    assert np.allclose(data_obj[0][-1], np.array([-0.73887169, -0.43727184, -0.43727184]))
+    assert np.allclose(data_obj[0][-1], data_obj[1][-1])
+    assert np.allclose(data_obj[0][0], data_obj[1][0])
 
 
 @pytest.mark.parametrize('vasprun_parser', [('relax', {})], indirect=True)
@@ -184,10 +184,10 @@ def test_traj_forces_result_relax(fresh_aiida_env, vasprun_parser):
     # test shape of array
     assert data_obj_arr.shape == (19, 8, 3)
     # test a few entries (first and last atom)
-    assert np.all(data_obj_arr[0][0] == np.array([-2.42632080e-01, 0.0, 0.0]))
-    assert np.all(data_obj_arr[0][-1] == np.array([-7.38879520e-01, -4.37063010e-01, -4.37063010e-01]))
-    assert np.all(data_obj_arr[-1][0] == np.array([1.55852000e-03, 0.0, 0.0]))
-    assert np.all(data_obj_arr[-1][-1] == np.array([-1.75970000e-03, 1.12150000e-04, 1.12150000e-04]))
+    assert np.allclose(data_obj_arr[0][0], np.array([-2.42632080e-01, 0.0, 0.0]))
+    assert np.allclose(data_obj_arr[0][-1], np.array([-7.38879520e-01, -4.37063010e-01, -4.37063010e-01]))
+    assert np.allclose(data_obj_arr[-1][0], np.array([1.55852000e-03, 0.0, 0.0]))
+    assert np.allclose(data_obj_arr[-1][-1], np.array([-1.75970000e-03, 1.12150000e-04, 1.12150000e-04]))
 
 
 @pytest.mark.parametrize('vasprun_parser', [('relax', {})], indirect=True)
@@ -209,10 +209,10 @@ def test_unitcells_result_relax(fresh_aiida_env, vasprun_parser):
     # test shape of array
     assert data_obj_arr.shape == (19, 3, 3)
     # test a few entries (first and last vector)
-    assert np.all(data_obj_arr[0][0] == np.array([5.46503124e+00, 0.0, 0.0]))
-    assert np.all(data_obj_arr[0][-1] == np.array([0.0, 0.0, 5.46503124e+00]))
-    assert np.all(data_obj_arr[-1][0] == np.array([5.46702248e+00, 0.0, 0.0]))
-    assert np.all(data_obj_arr[-1][-1] == np.array([0.0, 2.19104000e-03, 5.46705225e+00]))
+    assert np.allclose(data_obj_arr[0][0], np.array([5.46503124e+00, 0.0, 0.0]))
+    assert np.allclose(data_obj_arr[0][-1], np.array([0.0, 0.0, 5.46503124e+00]))
+    assert np.allclose(data_obj_arr[-1][0], np.array([5.46702248e+00, 0.0, 0.0]))
+    assert np.allclose(data_obj_arr[-1][-1], np.array([0.0, 2.19104000e-03, 5.46705225e+00]))
 
 
 @pytest.mark.parametrize('vasprun_parser', [('relax', {})], indirect=True)
@@ -234,10 +234,10 @@ def test_positions_result_relax(fresh_aiida_env, vasprun_parser):
     # test shape of array
     assert data_obj_arr.shape == (19, 8, 3)
     # test a few entries (first and last atom)
-    assert np.all(data_obj_arr[0][0] == np.array([0.0, 0.0, 0.0]))
-    assert np.all(data_obj_arr[0][-1] == np.array([0.75, 0.75, 0.25]))
-    assert np.all(data_obj_arr[-1][0] == np.array([-0.00621692, 0.0, 0.0]))
-    assert np.all(data_obj_arr[-1][-1] == np.array([0.7437189, 0.74989833, 0.24989833]))
+    assert np.allclose(data_obj_arr[0][0], np.array([0.0, 0.0, 0.0]))
+    assert np.allclose(data_obj_arr[0][-1], np.array([0.75, 0.75, 0.25]))
+    assert np.allclose(data_obj_arr[-1][0], np.array([-0.00621692, 0.0, 0.0]))
+    assert np.allclose(data_obj_arr[-1][-1], np.array([0.7437189, 0.74989833, 0.24989833]))
 
 
 @pytest.mark.parametrize('vasprun_parser', [('dielectric', {})], indirect=True)
@@ -262,13 +262,13 @@ def test_dielectrics(fresh_aiida_env, vasprun_parser):
     assert real.shape == (1000, 6)
     assert energy.shape == (1000,)
     # test a few entries
-    assert np.all(imag[0] == np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
-    assert np.all(imag[500] == np.array([0.0933, 0.0924, 0.0924, 0.0, 0.0082, 0.0]))
+    assert np.allclose(imag[0], np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
+    assert np.allclose(imag[500], np.array([0.0933, 0.0924, 0.0924, 0.0, 0.0082, 0.0]))
 
-    assert np.all(imag[999] == np.array([0.0035, 0.0035, 0.0035, 0.0, 0.0, 0.0]))
-    assert np.all(real[0] == np.array([12.0757, 11.4969, 11.4969, 0.0, 0.6477, 0.0]))
-    assert np.all(real[500] == np.array([-0.5237, -0.5366, -0.5366, 0.0, 0.0134, 0.0]))
-    assert np.all(real[999] == np.array([6.57100000e-01, 6.55100000e-01, 6.55100000e-01, 0.0, -1.00000000e-04, 0.0]))
+    assert np.allclose(imag[999], np.array([0.0035, 0.0035, 0.0035, 0.0, 0.0, 0.0]))
+    assert np.allclose(real[0], np.array([12.0757, 11.4969, 11.4969, 0.0, 0.6477, 0.0]))
+    assert np.allclose(real[500], np.array([-0.5237, -0.5366, -0.5366, 0.0, 0.0134, 0.0]))
+    assert np.allclose(real[999], np.array([6.57100000e-01, 6.55100000e-01, 6.55100000e-01, 0.0, -1.00000000e-04, 0.0]))
     assert energy[500] == pytest.approx(10.2933)
 
 
@@ -316,9 +316,9 @@ def test_born(fresh_aiida_env, vasprun_parser):
     # test shape of array
     assert born.shape == (8, 3, 3)
     # test a few entries
-    assert np.all(born[0][0] == np.array([6.37225000e-03, 0.0, 0.0]))
-    assert np.all(born[0][-1] == np.array([-4.21760000e-04, -2.19570210e-01, 3.20709600e-02]))
-    assert np.all(born[4][0] == np.array([1.68565200e-01, -2.92058000e-02, -2.92058000e-02]))
+    assert np.allclose(born[0][0], np.array([6.37225000e-03, 0.0, 0.0]))
+    assert np.allclose(born[0][-1], np.array([-4.21760000e-04, -2.19570210e-01, 3.20709600e-02]))
+    assert np.allclose(born[4][0], np.array([1.68565200e-01, -2.92058000e-02, -2.92058000e-02]))
 
 
 @pytest.mark.parametrize('vasprun_parser', [('basic', {})], indirect=True)
@@ -392,8 +392,8 @@ def test_pdos(fresh_aiida_env, vasprun_parser):
     assert dos.shape == (8, 1000, 9)
     assert energy.shape == (1000,)
     # test a few entries
-    assert np.all(dos[3, 500] == np.array([0.0770, 0.0146, 0.0109, 0.0155, 0.0, 0.0, 0.0, 0.0, 0.0]))
-    assert np.all(dos[7, 500] == np.array([0.0747, 0.0121, 0.0092, 0.0116, 0.0, 0.0, 0.0, 0.0, 0.0]))
+    assert np.allclose(dos[3, 500], np.array([0.0770, 0.0146, 0.0109, 0.0155, 0.0, 0.0, 0.0, 0.0, 0.0]))
+    assert np.allclose(dos[7, 500], np.array([0.0747, 0.0121, 0.0092, 0.0116, 0.0, 0.0, 0.0, 0.0, 0.0]))
     assert energy[500] == pytest.approx(0.01)
 
 
@@ -415,9 +415,9 @@ def test_projectors(fresh_aiida_env, vasprun_parser):
     # test shape of array
     assert proj.shape == (8, 64, 21, 9)
     # test a few entries
-    assert np.all(proj[0, 0, 5] == np.array([0.0, 0.012, 0.0123, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
-    assert np.all(proj[7, 0, 5] == np.array([0.1909, 0.0001, 0.0001, 0.0001, 0.0, 0.0, 0.0, 0.0, 0.0]))
-    assert np.all(proj[4, 3, 5] == np.array([0.2033, 0.0001, 0.0001, 0.0001, 0.0, 0.0, 0.0, 0.0, 0.0]))
+    assert np.allclose(proj[0, 0, 5], np.array([0.0, 0.012, 0.0123, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
+    assert np.allclose(proj[7, 0, 5], np.array([0.1909, 0.0001, 0.0001, 0.0001, 0.0, 0.0, 0.0, 0.0, 0.0]))
+    assert np.allclose(proj[4, 3, 5], np.array([0.2033, 0.0001, 0.0001, 0.0001, 0.0, 0.0, 0.0, 0.0, 0.0]))
 
 
 @pytest.mark.parametrize('vasprun_parser', [('basic', {})], indirect=True)
@@ -689,16 +689,22 @@ def test_hessian(fresh_aiida_env, vasprun_parser):
     # test shape
     assert hessian.shape == (24, 24)
     # test a few entries
-    assert np.all(hessian[0] == np.array([
-        -4.63550410e-01, 0.00000000e+00, 0.00000000e+00, -5.91774100e-02, 0.00000000e+00, 0.00000000e+00, 3.09711000e-02, 0.00000000e+00,
-        0.00000000e+00, 3.20435400e-02, 0.00000000e+00, 0.00000000e+00, 1.15129840e-01, -8.16138200e-02, 8.17234700e-02, 1.14879520e-01,
-        8.11324800e-02, 8.27409500e-02, 1.14879520e-01, -8.11324800e-02, -8.27409500e-02, 1.15129840e-01, 8.16138200e-02, -8.17234700e-02
-    ]))
-    assert np.all(hessian[-2] == np.array([
-        8.16138200e-02, 1.15195590e-01, -8.38411100e-02, -8.17234700e-02, 1.14875090e-01, -8.53388100e-02, 3.46686900e-02, 7.00672700e-02,
-        2.54288300e-02, -8.26222700e-02, 1.16185510e-01, 7.95575600e-02, -3.05970000e-04, 3.16827300e-02, 2.86379000e-03, 5.42080000e-04,
-        3.27613500e-02, 1.12576000e-03, -1.34305000e-03, -5.86811100e-02, 2.83374000e-03, 4.91688400e-02, -4.22101090e-01, 5.73736900e-02
-    ]))
+    assert np.allclose(
+        hessian[0],
+        np.array([
+            -4.63550410e-01, 0.00000000e+00, 0.00000000e+00, -5.91774100e-02, 0.00000000e+00, 0.00000000e+00, 3.09711000e-02,
+            0.00000000e+00, 0.00000000e+00, 3.20435400e-02, 0.00000000e+00, 0.00000000e+00, 1.15129840e-01, -8.16138200e-02, 8.17234700e-02,
+            1.14879520e-01, 8.11324800e-02, 8.27409500e-02, 1.14879520e-01, -8.11324800e-02, -8.27409500e-02, 1.15129840e-01,
+            8.16138200e-02, -8.17234700e-02
+        ]))
+    assert np.allclose(
+        hessian[-2],
+        np.array([
+            8.16138200e-02, 1.15195590e-01, -8.38411100e-02, -8.17234700e-02, 1.14875090e-01, -8.53388100e-02, 3.46686900e-02,
+            7.00672700e-02, 2.54288300e-02, -8.26222700e-02, 1.16185510e-01, 7.95575600e-02, -3.05970000e-04, 3.16827300e-02,
+            2.86379000e-03, 5.42080000e-04, 3.27613500e-02, 1.12576000e-03, -1.34305000e-03, -5.86811100e-02, 2.83374000e-03,
+            4.91688400e-02, -4.22101090e-01, 5.73736900e-02
+        ]))
 
 
 @pytest.mark.parametrize('vasprun_parser', [('disp', {})], indirect=True)
@@ -721,17 +727,22 @@ def test_dynmat(fresh_aiida_env, vasprun_parser):
     assert dynvec.shape == (24, 24)
     assert dyneig.shape == (24,)
     # test a few entries
-    assert np.all(dynvec[0] == np.array([
-        7.28517310e-17, 7.25431601e-02, -4.51957676e-02, 1.15412776e-16, 4.51957676e-02, -7.25431601e-02, -1.37347223e-16, 5.16257351e-01,
-        -5.16257351e-01, 8.16789156e-17, 8.95098005e-02, -8.95098005e-02, -4.43838008e-17, -6.38031134e-02, 6.38031134e-02, -1.80132830e-01,
-        -2.97969516e-01, 2.97969516e-01, 1.80132830e-01, -2.97969516e-01, 2.97969516e-01, -2.09989969e-16, -6.38031134e-02, 6.38031134e-02
-    ]))
-    assert np.all(dynvec[4] == np.array([
-        -5.29825122e-13, -2.41759046e-01, -3.28913434e-01, -5.30734671e-13, -3.28913434e-01, -2.41759046e-01, 3.26325910e-13,
-        -3.80807441e-02, -3.80807441e-02, -9.22956103e-13, -2.99868012e-01, -2.99868012e-01, 1.64418993e-01, 1.81002749e-01, 1.81002749e-01,
-        3.11984195e-13, 2.73349550e-01, 2.73349550e-01, 2.59853610e-13, 2.73349550e-01, 2.73349550e-01, -1.64418993e-01, 1.81002749e-01,
-        1.81002749e-01
-    ]))
+    assert np.allclose(
+        dynvec[0],
+        np.array([
+            7.28517310e-17, 7.25431601e-02, -4.51957676e-02, 1.15412776e-16, 4.51957676e-02, -7.25431601e-02, -1.37347223e-16,
+            5.16257351e-01, -5.16257351e-01, 8.16789156e-17, 8.95098005e-02, -8.95098005e-02, -4.43838008e-17, -6.38031134e-02,
+            6.38031134e-02, -1.80132830e-01, -2.97969516e-01, 2.97969516e-01, 1.80132830e-01, -2.97969516e-01, 2.97969516e-01,
+            -2.09989969e-16, -6.38031134e-02, 6.38031134e-02
+        ]))
+    assert np.allclose(
+        dynvec[4],
+        np.array([
+            -5.29825122e-13, -2.41759046e-01, -3.28913434e-01, -5.30734671e-13, -3.28913434e-01, -2.41759046e-01, 3.26325910e-13,
+            -3.80807441e-02, -3.80807441e-02, -9.22956103e-13, -2.99868012e-01, -2.99868012e-01, 1.64418993e-01, 1.81002749e-01,
+            1.81002749e-01, 3.11984195e-13, 2.73349550e-01, 2.73349550e-01, 2.59853610e-13, 2.73349550e-01, 2.73349550e-01, -1.64418993e-01,
+            1.81002749e-01, 1.81002749e-01
+        ]))
     assert dyneig[0] == pytest.approx(-1.36621537e+00)
     assert dyneig[4] == pytest.approx(-8.48939361e-01)
 
