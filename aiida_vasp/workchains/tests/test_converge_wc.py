42df23c0c824659089f8d3a8d4929a2a8d80f280
lan496 <kohei19950508@gmail.com>
2021-12-07 06:56:00 +0000

    Fix to read converge.pwcutoff input
    This commit fixes a plane-wave cutoff input for `ConvergeWorkChain`.
    Now, a plane-wave cutoff can be fixed during convergence tests by setting
    `converge.pwcutoff` input.

--- aiida_vasp/workchains/tests/test_converge_wc.py
+++ aiida_vasp/workchains/tests/test_converge_wc.py
@@ -167,3 +167,76 @@ def test_converge_wc_pw(fresh_aiida_env, vasp_params, potentials, mock_vasp):
         np.testing.assert_equal(_encut, 300)
     except AttributeError:
         pytest.fail('pwcutoff_recommended does not have the expected format')
+
+def test_converge_wc_kgrid(fresh_aiida_env, potentials, mock_vasp):
+    """Test convergence workflow using mock code."""
+    from aiida.orm import Code
+    from aiida.plugins import WorkflowFactory
+    from aiida.engine import run
+
+    workchain = WorkflowFactory('vasp.converge')
+
+    mock_vasp.store()
+    create_authinfo(computer=mock_vasp.computer).store()
+
+    structure = PoscarParser(file_path=data_path('test_converge_wc/kgrid/8_8_8', 'inp', 'POSCAR')).structure
+    parameters = IncarParser(file_path=data_path('test_converge_wc/kgrid/8_8_8', 'inp', 'INCAR')).incar
+    parameters['system'] = 'test-case:test_converge_wc'
+    pwcutoff = parameters['encut']
+    parameters = {
+        'incar': {
+            k: v for k, v in parameters.items() if k not in ['isif', 'ibrion', 'encut', 'nsw']
+        },
+    }
+
+    restart_clean_workdir = get_data_node('bool', False)
+    restart_clean_workdir.store()
+
+    inputs = AttributeDict()
+    inputs.code = Code.get_from_string('mock-vasp@localhost')
+    inputs.structure = structure
+    inputs.parameters = get_data_node('dict', dict=parameters)
+    inputs.potential_family = get_data_node('str', POTCAR_FAMILY_NAME)
+    inputs.potential_mapping = get_data_node('dict', dict=POTCAR_MAP)
+    inputs.options = get_data_node('dict',
+                                   dict={
+                                       'queue_name': 'None',
+                                       'resources': {
+                                           'num_machines': 1,
+                                           'num_mpiprocs_per_machine': 1
+                                       },
+                                   })
+    inputs.max_iterations = get_data_node('int', 1)
+    inputs.clean_workdir = get_data_node('bool', False)
+    relax = AttributeDict()
+    relax.perform = get_data_node('bool', False)
+    inputs.relax = relax
+    converge = AttributeDict()
+    converge.pwcutoff = get_data_node('float', pwcutoff)
+    converge.k_dense = get_data_node('float', 0.21)  # 10x10x10 mesh
+    converge.k_course = get_data_node('float', 0.27)  # 8x8x8 mesh
+    converge.k_samples = get_data_node('int', 1)
+    converge.testing = get_data_node('bool', True)
+    inputs.converge = converge
+    inputs.verbose = get_data_node('bool', True)
+    results, node = run.get_node(workchain, **inputs)
+
+    assert node.exit_status == 0
+    assert 'converge' in results
+    converge = results['converge']
+    assert 'data' in converge
+    conv_data = converge['data']
+    try:
+        conv_data = conv_data.get_array('kpoints_regular')
+    except KeyError:
+        pytest.fail('Did not find kpoints_regular in converge.data')
+    # this workflow should check 8x8x8 and 10x10x10 meshs
+    np.testing.assert_allclose(conv_data[0][:4], [8.0, 8.0, 8.0, pwcutoff])
+    np.testing.assert_allclose(conv_data[1][:4], [10.0, 10.0, 10.0, pwcutoff])
+
+    assert 'kpoints_recommended' in converge
+    try:
+        _kpoints, _ = converge['kpoints_recommended'].get_kpoints_mesh()
+        np.testing.assert_equal(_kpoints, [10.0, 10.0, 10.0])
+    except AttributeError:
+        pytest.fail('kpoints_recommended does not have the expected format')
