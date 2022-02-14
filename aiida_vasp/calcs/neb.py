1d78d29afb5cc6ec153fbdf2fcbeca842cca4274
Bonan Zhu <zhubonan@outlook.com>
2022-02-14 21:18:41 +0000

    Restore CifData compatibility

--- aiida_vasp/calcs/neb.py
+++ aiida_vasp/calcs/neb.py
@@ -275,6 +275,11 @@ class VaspNEBCalculation(VaspCalculation):
         last_num_sites = None
         for structure in list(self.inputs.neb_images.values()) + [self.inputs.initial_structure, self.inputs.final_structure]:
             num_sites = len(structure.sites)
+
+            # Convert to StructureData from CifData on demand....
+            if not hasattr(structure, 'get_pymatgen'):
+                structure = get_data_node('structure', ase=structure.get_ase())
+
             order = ordered_unique_symbols(structure)
             if last_order is not None and order != last_order:
                 raise InputValidationError('The input structures have non-euqal element orders.')
