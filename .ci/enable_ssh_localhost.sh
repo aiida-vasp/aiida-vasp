d2f6d2fb07a84b99270fe2ccbf8c92deffccd9a9
Jonathan Chico <jonathan.chico@sandvik.com>
2024-04-18 11:17:05 +0200

    Separeting the CI and CD scripts to have a clearer separation on what should belong to what

--- .ci/enable_ssh_localhost.sh
+++
@@ -1,8 +0,0 @@
-et -ev
-
-ssh-keygen -q -t rsa -b 4096 -m PEM -N "" -f "${HOME}/.ssh/id_rsa"
-ssh-keygen -y -f "${HOME}/.ssh/id_rsa" >> "${HOME}/.ssh/authorized_keys"
-ssh-keyscan -H localhost >> "${HOME}/.ssh/known_hosts"
-
-# The permissions on the GitHub runner are 777 which will cause SSH to refuse the keys and cause authentication to fail
-chmod 755 "${HOME}"
