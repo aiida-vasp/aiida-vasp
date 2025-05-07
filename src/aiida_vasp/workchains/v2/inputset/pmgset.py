da40026825b208c4a5240502fa2145b6924362f7
Bonan Zhu <zhubonan@outlook.com>
2025-05-07 22:12:37 +0800

    Added pymatgen compatible inputset generator

---
+++ src/aiida_vasp/workchains/v2/inputset/pmgset.py
@@ -0,0 +1,114 @@
+"""
+Module for using pymatgen.io.vasp.sets based input sets.
+"""
+
+from typing import Union
+
+from aiida import orm
+
+from .base import InputSet
+
+
+class PymatgenInputSet(InputSet):
+    """
+    Input set using pymatgen.io.vasp.sets.
+    Provides basic compatibility with pymatgen sets.
+    """
+
+    def __init__(self, set_name: str, overrides=None, verbose=None, pmg_kwargs=None):
+        """
+        Instantiate a PymatgenInputSet.
+
+        :param overrides: A dictionary of overrides for the input set.
+        :param verbose: If True, print additional information.
+        :param pmg_kwargs: A dictionary of keyword arguments to pass to the pymatgen input set.
+        """
+        super().__init__(set_name, overrides=overrides, verbose=verbose)
+        self._pmg_kwargs = pmg_kwargs or {}
+
+    def _load_data(self):
+        """Load the data for the input set."""
+        try:
+            import pymatgen.io.vasp.sets as pmg_sets
+        except ImportError:
+            raise ImportError('pymatgen has to be installed to use this feature.')
+
+        self._pmg_class = getattr(pmg_sets, self.set_name)
+
+    def get_input_dict(self, structure, raw_python=True) -> Union[dict, orm.Dict]:
+        """
+        Compute the input parameters for a VASP calculation using pymatgen.io.vasp.sets.
+        """
+        ps = structure.get_pymatgen()
+        pmgset = self._pmg_class(ps, **self._pmg_kwargs)
+        incar_dict = {key.lower(): value for key, value in pmgset.incar.items()}
+        # Apply the overrides
+        for key, value in self.overrides.items():
+            if value is None:
+                if key in incar_dict:
+                    incar_dict.pop(key)
+            else:
+                incar_dict[key] = value
+        if raw_python:
+            return incar_dict
+
+        return orm.Dict(dict=incar_dict)
+
+    def get_pp_mapping(self, structure):
+        """Get the potential mapping used by the input set."""
+        ps = structure.get_pymatgen()
+        pmgset = self._pmg_class(ps, **self._pmg_kwargs)
+        return {p.element: p.symbol for p in pmgset.potcar}
+
+    def get_potcar_family(self):
+        """
+        Get the POTCAR family used by the input set.
+        Note that we replace `_` by `.`, so PBE_54 is PBE.54
+        """
+        return self._pmg_class.CONFIG['POTCAR_FUNCTIONAL'].replace('_', '.')
+
+    def get_kpoints(self, structure) -> orm.KpointsData:
+        """
+        Return a KpointsData object for the given structure.
+        """
+        ps = structure.get_pymatgen()
+        pmgset = self._pmg_class(ps, **self._pmg_kwargs)
+        # Currently only supports Gamma and Monkhorst-Pack
+        kpoints_data = pmg_kpoints2kpointsdata(pmgset.kpoints)
+        kpoints_data.set_cell_from_structure(structure)
+        return kpoints_data
+
+    def get_kpoints_spacing(self):
+        """Get the spacing of the kpoints used by the input set."""
+        raise NotImplementedError
+
+
+def pmg_kpoints2kpointsdata(pmg_kpoints):
+    """Convert a pymatgen Kpoints object to an AiiDA KpointsData object."""
+    from pymatgen.io.vasp.inputs import KpointsSupportedModes
+
+    # Currently only supports Gamma and Monkhorst-Pack
+    style = pmg_kpoints.style
+    mesh = pmg_kpoints.kpts[0]
+    if style == KpointsSupportedModes.Gamma:
+        # aiida-vasp defaults to use Gamma-centering mode when constructing the KPOINTS file
+        gamma_shifts = (0, 0, 0)
+    elif style == KpointsSupportedModes.Monkhorst:
+        # If a MP grid is supplied, we just add a -0.5 shift to the gamma-centering grid to make it
+        # equivalent to a MP centred grid
+        # See https://www.vasp.at/wiki/index.php/KPOINTS
+        gamma_shifts = []
+        for i in mesh:
+            if i % 2 == 1:
+                # Odd division - do nothing
+                gamma_shifts.append(0)
+            else:
+                # Even division - add shifts
+                gamma_shifts.append(-0.5)
+    else:
+        raise ValueError(f'Unsupported kpoint style: {style}')
+    shifts = pmg_kpoints.kpts_shift
+    # Construct AiiDA KpointsData object
+    kpoints = orm.KpointsData()
+    kpoints.set_kpoints_mesh(mesh, offset=[i + j for i, j in zip(shifts, gamma_shifts)])
+    return kpoints
--- tests/workchains/v2/test_inputset.py
+++ tests/workchains/v2/test_inputset.py
@@ -2,11 +2,13 @@
 Test for input set specifications
 """
 
+import numpy as np
 import pytest
 from aiida import orm
 from ase.build import bulk
 
 from aiida_vasp.workchains.v2.inputset.base import InputSet
+from aiida_vasp.workchains.v2.inputset.pmgset import PymatgenInputSet
 from aiida_vasp.workchains.v2.inputset.vaspsets import VASPInputSet
 
 # pylint:disable=redefined-outer-name,unused-argument
@@ -16,6 +18,12 @@ from aiida_vasp.workchains.v2.inputset.vaspsets import VASPInputSet
 def fe_atoms(aiida_profile):
     """Get a Fe atoms"""
     return orm.StructureData(ase=bulk('Fe', 'fcc', 5.0))
+
+
+@pytest.fixture
+def feo_atoms(aiida_profile):
+    """Get a FeO atoms in rocksalt structure"""
+    return orm.StructureData(ase=bulk('FeO', 'rocksalt', 5.0))
 
 
 @pytest.fixture
@@ -55,3 +63,47 @@ def test_kpoints(aiida_profile, fe_atoms):
     inset = VASPInputSet('MITRelaxSet')
     kpoints = inset.get_kpoints(fe_atoms, 0.05)
     assert kpoints.get_kpoints_mesh()[0][0] == 7
+
+
+def test_pmg_kpoints(aiida_profile, fe_atoms):
+    """Test generating kpoints"""
+    inset = PymatgenInputSet('MPRelaxSet')
+    kpoints = inset.get_kpoints(fe_atoms)
+    assert kpoints.get_kpoints_mesh()[0][0] == 7
+    assert kpoints.get_kpoints_mesh()[1][0] == 0
+
+    inset = PymatgenInputSet('MPRelaxSet')
+    scaled_fe_atoms = fe_atoms.get_ase()
+    scaled_fe_atoms.set_cell(scaled_fe_atoms.get_cell() * 1.2)
+    # Breaks the symmetry so it is not face centred
+    scaled_fe_atoms.cell += np.random.rand(3, 3) * 0.01
+    scaled_fe_atoms = orm.StructureData(ase=scaled_fe_atoms)
+    kpoints = inset.get_kpoints(scaled_fe_atoms)
+    assert kpoints.get_kpoints_mesh()[0][0] == 6
+    assert kpoints.get_kpoints_mesh()[1][0] == -0.5
+
+
+def test_pmg(fe_atoms, feo_atoms):
+    """Test pymatgen inputsets"""
+    vpmgset = PymatgenInputSet('MPRelaxSet', overrides={'ediff': 1.0, 'nsw': None, 'ldautype': 3})
+    out = vpmgset.get_input_dict(fe_atoms)
+    assert 'nsw' not in out
+    assert out['ediff'] == 1.0
+    assert out['ldautype'] == 3
+    assert 'ldau' not in out
+    assert out['ismear'] == -5
+
+    vpmgset = PymatgenInputSet('MPRelaxSet', overrides={'ediff': 1.0, 'nsw': None})
+    out = vpmgset.get_input_dict(feo_atoms)
+    assert 'ldau' in out
+
+    # Check equivalence with pymatgen
+    from pymatgen.io.vasp.sets import MPRelaxSet
+
+    for structure in feo_atoms, fe_atoms:
+        pmgset = MPRelaxSet(structure.get_pymatgen())
+        vpmgset = PymatgenInputSet('MPRelaxSet')
+        out = vpmgset.get_input_dict(structure)
+        assert dict(pmgset.incar) == {key.upper(): value for key, value in out.items()}
+        assert pmgset.kpoints.kpts[0] == tuple(vpmgset.get_kpoints(structure).get_kpoints_mesh()[0])
+        assert {p.element: p.symbol for p in pmgset.potcar} == vpmgset.get_pp_mapping(structure)
